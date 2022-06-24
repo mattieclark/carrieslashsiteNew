c9d11060422d651afa444b7c1d4876bbe1eba541
mattieclark <101110734+mattieclark@users.noreply.github.com>
2022-06-24 19:56:35 +0100

    working template
    folder tidy, mix and blend banner + footer. Still require html content.

--- lashes site new edit/script.js
+++ lashes site new edit/script.js
@@ -56,7 +56,7 @@ banner = document.getElementById("nav-bar");
 
 (function (){
     function getPosition(e){
-        if (e.clientY < 75) {
+        if (e.clientY < 10) {
         banner.classList.add("active")
         }
     }
@@ -77,32 +77,6 @@ navMenu = document.querySelector(".nav-menu");
 
 
 
-// const sections = document.querySelectorAll('section[id]')
-
-// function scrollActive() {
-//     const scrollY = window.pageYOffset
-
-//     sections.forEach(current => {
-//         const sectionHeight = current.offsetHeight
-//         const sectionTop = current.offsetTop - 50;
-//         sectionId = current.getAttribute('id')
-
-//         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-//             document.querySelector('.nav__item a[href*=' + sectionId + ']').classList.add('active')
-//         } else {
-//             document.querySelector('.nav__item a[href*=' + sectionId + ']').classList.remove('active')
-//         }
-//     })
-// }
-
-// /*===== SCROLL REVEAL ANIMATION =====*/
-// const sr = ScrollReveal({
-//     origin: 'top',
-//     distance: '60px',
-//     duration: 2000,
-//     delay: 200,
-// //     reset: true
-// });
 
 
 
